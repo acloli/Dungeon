d5982a2469647bc884fde057c54e4b2a53031684
acloli <h@acloli.com>
2026-06-07 18:10:27 +0900

    add: encounter formation import artifacts

---
+++ DungeonUnity/Assets/Scripts/Generated/MasterData/EncounterFormationMaster.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using TFramework.MasterData;
+
+namespace Game.MasterData.Generated
+{
+    public partial class EncounterFormationMaster
+    {
+        // ここに拡張メソッドや計算プロパティを記述する
+    }
+}
