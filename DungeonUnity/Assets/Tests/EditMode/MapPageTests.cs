bef852f8e46d2b08e4e766ac18069704c06af592
acloli <h@acloli.com>
2026-07-05 03:03:36 +0900

    test: cover map page order and interactable mapping

--- DungeonUnity/Assets/Tests/EditMode/MapPageTests.cs
+++ DungeonUnity/Assets/Tests/EditMode/MapPageTests.cs
@@ -78,6 +78,91 @@ namespace Dungeon.Tests.EditMode
             AssertTemplateColor(prefab, "BossNodeTemplate", new Color32(178, 123, 54, 255));
         }
 
+        [Test]
+        public void BuildMapButtons_WithSameTypeNodes_PreservesVisualOrder()
+        {
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(MapPagePrefabPath);
+            GameObject instance = Object.Instantiate(prefab);
+
+            try
+            {
+                MapPage page = instance.GetComponent<MapPage>();
+                page.BuildMapButtons(
+                    new[]
+                    {
+                        CreateNode(1, InGameNodeType.Battle, "Top"),
+                        CreateNode(1, InGameNodeType.Battle, "Bottom")
+                    },
+                    null);
+
+                string[] labels = GetActiveButtonLabels(page);
+
+                Assert.That(labels, Is.EqualTo(new[] { "1.Top", "1.Bottom" }));
+            }
+            finally
+            {
+                Object.DestroyImmediate(instance);
+            }
+        }
+
+        [Test]
+        public void BuildMapButtons_WithMixedTypeNodes_PreservesVisualOrder()
+        {
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(MapPagePrefabPath);
+            GameObject instance = Object.Instantiate(prefab);
+
+            try
+            {
+                MapPage page = instance.GetComponent<MapPage>();
+                page.BuildMapButtons(
+                    new[]
+                    {
+                        CreateNode(2, InGameNodeType.RestShop, "Rest"),
+                        CreateNode(2, InGameNodeType.Event, "Event")
+                    },
+                    null);
+
+                string[] labels = GetActiveButtonLabels(page);
+
+                Assert.That(labels, Is.EqualTo(new[] { "2.Rest", "2.Event" }));
+            }
+            finally
+            {
+                Object.DestroyImmediate(instance);
+            }
+        }
+
+        [Test]
+        public void SetMapButtonInteractable_MatchesDisplayedButtonOrder()
+        {
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(MapPagePrefabPath);
+            GameObject instance = Object.Instantiate(prefab);
+
+            try
+            {
+                MapPage page = instance.GetComponent<MapPage>();
+                page.BuildMapButtons(
+                    new[]
+                    {
+                        CreateNode(1, InGameNodeType.RestShop, "Rest"),
+                        CreateNode(1, InGameNodeType.Event, "Event")
+                    },
+                    null);
+                page.SetMapButtonInteractable(new[] { 0 });
+
+                Transform[] activeChildren = GetActiveChildren(page);
+
+                Assert.That(ReadLabel(activeChildren[0]), Is.EqualTo("1.Rest"));
+                Assert.That(activeChildren[0].GetComponent<Button>().interactable, Is.True);
+                Assert.That(ReadLabel(activeChildren[1]), Is.EqualTo("1.Event"));
+                Assert.That(activeChildren[1].GetComponent<Button>().interactable, Is.False);
+            }
+            finally
+            {
+                Object.DestroyImmediate(instance);
+            }
+        }
+
         private static T GetSerializedReference<T>(Object target, string propertyName) where T : Object
         {
             SerializedObject serialized = new SerializedObject(target);
@@ -101,6 +186,39 @@ namespace Dungeon.Tests.EditMode
             Image image = template.GetComponent<Image>();
             Assert.That(image, Is.Not.Null, $"{templateName} should have an Image.");
             Assert.That((Color32)image.color, Is.EqualTo(expectedColor));
+        }
+
+        private static RuntimeMapNode CreateNode(int floor, InGameNodeType nodeType, string displayName)
+        {
+            return new RuntimeMapNode(
+                floor,
+                displayName,
+                floor,
+                nodeType,
+                displayName,
+                string.Empty,
+                System.Array.Empty<int>());
+        }
+
+        private static string[] GetActiveButtonLabels(MapPage page)
+        {
+            Transform[] activeChildren = GetActiveChildren(page);
+            return activeChildren.Select(ReadLabel).ToArray();
+        }
+
+        private static Transform[] GetActiveChildren(MapPage page)
+        {
+            Transform nodeRoot = GetSerializedReference<Transform>(page, "_nodeRoot");
+            return nodeRoot.Cast<Transform>().Where(child => child.gameObject.activeSelf).ToArray();
+        }
+
+        private static string ReadLabel(Transform child)
+        {
+            Component label = child.Find("Label")
+                .GetComponents<Component>()
+                .First(component => component.GetType().Name == "TFTextUGUI");
+            SerializedObject serialized = new SerializedObject(label);
+            return serialized.FindProperty("m_text").stringValue;
         }
     }
 }
