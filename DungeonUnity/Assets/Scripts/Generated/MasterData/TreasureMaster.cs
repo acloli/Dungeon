35a36a0ce45490c17bebf2d27e26367851ac3916
acloli <h@acloli.com>
2026-07-11 03:20:07 +0900

    add: generate treasure masterdata types

---
+++ DungeonUnity/Assets/Scripts/Generated/MasterData/TreasureMaster.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using TFramework.MasterData;
+
+namespace Game.MasterData.Generated
+{
+    public partial class TreasureMaster
+    {
+        // ここに拡張メソッドや計算プロパティを記述する
+    }
+}
